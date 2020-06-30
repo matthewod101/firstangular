b8c0114c4780412f1b4ffca1fb2beb52dfad5af6
matthewod101 <38329959+matthewod101@users.noreply.github.com>
2020-06-30 13:40:24 -0400

    Adding dashboard and nav

--- src/app/components/shopping/shopping.component.ts
+++ src/app/components/shopping/shopping.component.ts
@@ -9,13 +9,28 @@ import { ShoppingItem } from '../../models';
 export class ShoppingComponent implements OnInit {
 
   items: ShoppingItem[] = [
-    { description: 'Shampoo' },
-    { description: 'Beer' },
-    { description: 'More beer' }
+    { description: 'Shampoo', purchased: false },
+    { description: 'Beer', purchased: true },
+    { description: 'More beer', purchased: false }
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addItem(itemEl: HTMLInputElement): void {
+    console.log(itemEl);
+    const itemToAdd: ShoppingItem = {
+      description: itemEl.value,
+      purchased: false
+    };
+    this.items = [itemToAdd, ...this.items]; // ... spreads the elements of the included array in position for the new array.
+    itemEl.value = '';
+    itemEl.focus();
+  }
+
+  markPurchased(item: ShoppingItem): void {
+    item.purchased = true;
+  }
 }
+
