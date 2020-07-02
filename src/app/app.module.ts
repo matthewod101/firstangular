985daad6bac162a5c2e450776d6b673120da0a52
matthewod101 <38329959+matthewod101@users.noreply.github.com>
2020-07-02 15:50:20 -0400

    End of Thursday, connected to API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MediaContainerComponent } from './components/media-container/media-cont
 import { MediaEntryComponent } from './components/media-entry/media-entry.component';
 import { MediaListComponent } from './components/media-list/media-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument(),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
